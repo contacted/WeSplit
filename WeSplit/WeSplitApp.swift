8af650217babec96364d32469d24c58f0efdfa73
Eugene <contact.duk@gmail.com>
2022-02-20 17:08:21 +0300

    Adding initial source code

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eugene on 11.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
